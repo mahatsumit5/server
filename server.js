084c079b8a32f4a8a40498d588f6bb59b99070d7
mahat sumit <mahatsumit5@gmail.com>
2023-09-06 14:49:09 +1000

    extract api created using openai

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ connectToDatabase();
 import userRouter from "./src/router/userRouter.js";
 app.use("/api/v1/user", userRouter);
 import skillsRouter from "./src/router/skillsRouter.js";
+import extractRouter from "./src/router/extractRouter.js";
 app.use("/api/v1/skill", skillsRouter);
+app.use("/api/v1/extract", extractRouter);
 app.get("/", (req, res) => {
   res.json({ status: "success", message: "server is running" });
 });
